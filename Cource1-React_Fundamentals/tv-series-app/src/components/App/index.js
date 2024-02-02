f498dadc4320b6b1c9ad00f65c5b6fcaa959c36d
John Kamperakis <johnkamperakis@gmail.com>
2024-02-02 09:58:11 +0200

    Section 1 : Containers

--- Cource1-React_Fundamentals/tv-series-app/src/components/App/index.js
+++ Cource1-React_Fundamentals/tv-series-app/src/components/App/index.js
@@ -1,19 +1,10 @@
 import React, {Component} from 'react';
 import 'whatwg-fetch';
 import Intro from '../Intro';
+import Series from '../../containers/Series';
 import './App.css';
 
 class App extends Component {
-  state = {
-    series: []
-  }
-
-  componentDidMount(){
-    fetch('http://api.tvmaze.com/search/shows?q=Vikings')
-      .then((response) => response.json())
-      .then(json => this.setState({series:json}))
-  }
-
   render(){
     return (
       <div className="App">
@@ -21,7 +12,7 @@ class App extends Component {
           <h1 className='App-title'> TV Series List </h1>
         </header>
         <Intro message="TEST TEST"/>
-        The length of series array - {this.state.series.length}
+        <Series />
       </div>
     );
   }
