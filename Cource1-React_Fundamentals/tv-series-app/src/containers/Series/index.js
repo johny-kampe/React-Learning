6b06d9f21243be58946d3b5b034bf5ff075963a3
John Kamperakis <johnkamperakis@gmail.com>
2024-02-02 12:10:59 +0200

    Section 2 : Images (Loader component)

--- Cource1-React_Fundamentals/tv-series-app/src/containers/Series/index.js
+++ Cource1-React_Fundamentals/tv-series-app/src/containers/Series/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import SeriesList from "../../components/SeriesList";
+import Loader from "../../components/Loader";
 
 class Series extends Component {
     state = {
@@ -27,15 +28,15 @@ class Series extends Component {
                         onChange={this.onSeriesInputChange} />
                 </div>
                 { 
-                    series.length === 0 && seriesName.trim() === '' 
+                    !isFetching && series.length === 0 && seriesName.trim() === '' 
                     && <p>Please enter series name into the input</p>
                 }
                 {
-                    series.length === 0 && seriesName.trim() !== ''
+                    !isFetching && series.length === 0 && seriesName.trim() !== ''
                     && <p>No TV series have been found with this name</p>
                 }
                 {
-                    isFetching && <p>Loading...</p>
+                    isFetching && <Loader />
                 }
                 {
                     !isFetching && <SeriesList list={this.state.series} />
